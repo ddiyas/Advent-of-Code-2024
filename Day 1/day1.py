f076a571e7c52120e71db3b6168488acd63a9c8e
Diya Shah <diya.kshah@gmail.com>
2024-12-02 15:12:01 -0500

    part 1

---
+++ Day 1/day1.py
@@ -0,0 +1,11 @@
+with open("Day 1\input.txt") as file:
+    pairs = [tuple(map(int, line.split())) for line in file]
+
+ids1, ids2 = zip(*pairs)
+
+ids1 = sorted(ids1)
+ids2 = sorted(ids2)
+
+diff_sum = sum(abs(a - b) for a, b in zip(ids1, ids2))
+
+print(diff_sum)
