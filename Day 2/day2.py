09fa0a6a2dfcbb4417b2f55f5b4a2d05b035921c
Diya Shah <diya.kshah@gmail.com>
2024-12-03 18:25:28 -0500

    day 2: part 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,16 +1,25 @@
+def is_safe(level):
+    for i in range(1, len(level)):
+        diff = level[i - 1] - level[i]
+        is_safe = True
+        if abs(diff) < 1 or abs(diff) > 3 or (level[0] - level[1]) * diff < 0:
+            return False
+    return True
+
+
 with open("Day 2\input.txt") as file:
     reports = [list(map(int, line.split())) for line in file]
 
 safe = 0
 
 for level in reports:
-    for i in range (1, len(level)):
-        diff = level[i - 1] - level[i]
-        is_safe = True
-        if abs(diff) < 1 or abs(diff) > 3 or (level[0] - level[1]) * diff < 0:
-            is_safe = False
-            break
-    if is_safe:
-        safe+=1
+    if is_safe(level):
+        safe += 1
+    else:
+        for i in range(0, len(level)):
+            new_level = level[:i] + level[i + 1 :]
+            if is_safe(new_level):
+                safe += 1
+                break
 
 print(safe)
