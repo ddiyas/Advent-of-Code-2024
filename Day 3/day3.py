9dba57d188d33cfab46fcac9cdaa9a1950e7e3db
Diya Shah <diya.kshah@gmail.com>
2024-12-04 14:59:53 -0500

    day 3: part 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -3,14 +3,20 @@ import re
 with open("Day 3\input.txt") as file:
     input_string = file.read()
 
-multiply = re.findall("mul\([0-9]+\,[0-9]+\)", input_string)
+multiply = re.findall("mul\([0-9]+\,[0-9]+\)|do\(\)|don't\(\)", input_string)
 
-answer = sum(
-    a * b
-    for a, b in tuple(
-        map(int, factor.replace("mul(", "").replace(")", "").split(","))
-        for factor in multiply
-    )
-)
+do = True
+answer = 0
+for factor in multiply:
+    if factor == "do()":
+        do = True
+    elif factor == "don't()":
+        do = False
+    else:
+        if do:
+            pair = tuple(
+                map(int, factor.replace("mul(", "").replace(")", "").split(","))
+            )
+            answer += pair[0] * pair[1]
 
 print(answer)
